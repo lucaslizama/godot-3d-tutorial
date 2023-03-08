2265e5e312f7687d248c52d06d94bf1b8e79e2a6
Lucas Lizama <lucaslizama4@gmail.com>
2023-03-08 02:21:57 -0300

    Innitial Commit

---
+++ scripts/Player.cs
@@ -0,0 +1,48 @@
+using Godot;
+
+public partial class Player : CharacterBody3D
+{
+  [Export]
+  public int Speed = 14;
+
+  [Export]
+  public int FallAcceleration = 75;
+
+  private Vector3 _velocity = Vector3.Zero;
+
+  public override void _PhysicsProcess(double delta)
+  {
+    var direction = Vector3.Zero;
+
+    if (Input.IsActionPressed("move_right"))
+    {
+      direction.X += 1f;
+    }
+    if (Input.IsActionPressed("move_left"))
+    {
+      direction.X -= 1f;
+    }
+    if (Input.IsActionPressed("move_back"))
+    {
+      direction.Z += 1f;
+    }
+    if (Input.IsActionPressed("move_forward"))
+    {
+      direction.Z -= 1f;
+    }
+
+    if (direction != Vector3.Zero)
+    {
+      direction = direction.Normalized();
+      GetNode<Node3D>("Pivot").LookAt(Position + direction, Vector3.Up);
+    }
+
+    _velocity.X = direction.X * Speed;
+    _velocity.Z = direction.Z * Speed;
+    _velocity.Y -= FallAcceleration * (float)delta;
+
+    Velocity = _velocity;
+
+    MoveAndSlide();
+  }
+}
